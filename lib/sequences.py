c0fb16ba766789d8ae2e57c3c3dd3f9556bc88e4
Tatiana Mathia <mathiatatiana@gmail.com>
2024-05-22 12:59:19 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
+
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    fibonacci_list = [0, 1]
+    while len(fibonacci_list) < length:
+        next_value = fibonacci_list[-1] + fibonacci_list[-2]
+        fibonacci_list.append(next_value)
+
+    print(fibonacci_list)
